03dfc570e45fa9e59cb400baefa86118b67a70db
amrik2508 <amrik.p@apalya.com>
2019-12-31 17:41:56 +0530

    Flash Screen text and backgroundimage API functionality added

--- app/src/main/java/com/amriksinghpadam/myplayer/MainFragment.java
+++ app/src/main/java/com/amriksinghpadam/myplayer/MainFragment.java
@@ -2,9 +2,14 @@ package com.amriksinghpadam.myplayer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
@@ -12,13 +17,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.amriksinghpadam.myplayer.api.APIConstent;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class MainFragment extends Fragment {
-    private LinearLayout layout1,layout2;
+    private View layout1,layout2;
     private Context context;
     private int tempCount=0;
+    private TextView songTitleView,videoTitleView;
+    private View songBannerView,videoBannerView;
+    private String songBannerUrl,videoBannerUrl;
 
     public MainFragment(Context context) {
         this.context = context;
@@ -30,7 +45,21 @@ public class MainFragment extends Fragment {
 
         layout1 = view.findViewById(R.id.linearLayout1);
         layout2 = view.findViewById(R.id.linearLayout2);
+        songTitleView = view.findViewById(R.id.songTitleId);
+        videoTitleView = view.findViewById(R.id.videoTitleId);
+        songBannerView = view.findViewById(R.id.songImageView);
+        videoBannerView = view.findViewById(R.id.videoImageView);
 
+        Bundle bundle = getArguments();
+        if(bundle!=null) {
+            String songTitle = bundle.getString(APIConstent.SONG_TITLE);
+            String videoTitle = bundle.getString(APIConstent.VIDEO_TITLE);
+            songBannerUrl = bundle.getString(APIConstent.SONG_BANNER);
+            videoBannerUrl = bundle.getString(APIConstent.VIDEO_BANNER);
+            songTitleView.setText(songTitle.toUpperCase());
+            videoTitleView.setText(videoTitle.toUpperCase());
+            loadBackgroundImage();
+        }
         layout1.setOnClickListener  (new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,8 +87,26 @@ public class MainFragment extends Fragment {
                 }
             }
         });
-
         return view;
+    }
+
+    public void loadBackgroundImage(){
+        Glide.with(context).load(songBannerUrl).into(new CustomTarget<Drawable>() {
+            @Override
+            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                songBannerView.setBackground(resource);
+            }
+            @Override
+            public void onLoadCleared(@Nullable Drawable placeholder) {}
+        });
+        Glide.with(context).load(videoBannerUrl).into(new CustomTarget<Drawable>() {
+            @Override
+            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                videoBannerView.setBackground(resource);
+            }
+            @Override
+            public void onLoadCleared(@Nullable Drawable placeholder) {}
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/amriksinghpadam/myplayer/api/APIConstent.java
@@ -0,0 +1,15 @@
+package com.amriksinghpadam.myplayer.api;
+
+public class APIConstent {
+
+    final public static String BASE_URL = "amriksinghpadam.com/MyplayerAPI/";
+    final public static String SCHEME = "http://";
+    final public static String SSL_SCHEME = "https://";
+    final public static String CONTENT_TYPE_URL_PARAM = "player_api/call_api_request.php?content_type_data=CONTENT+TYPE";
+    final public static String CONTENT_TYPE_KEY = "contenttype";
+    final public static String IMAGEURL = "imageurl";
+    final public static String SONG_TITLE = "songtitle";
+    final public static String VIDEO_TITLE = "videotitle";
+    final public static String SONG_BANNER = "songbanner";
+    final public static String VIDEO_BANNER = "videobanner";
+}
